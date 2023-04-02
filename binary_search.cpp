a74cfd46a6215d58a0ab146ccb21a5b86bc6e3ca
NandanChakraborty <nandancsebubt@gmail.com>
2023-04-02 20:35:53 +0600

    new code

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,14 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-int n,x,arr[10];
-cout<<"enter the number of  array elements :"<<endl;
-cin>>n;
+int main() {
+    int n;
+    cout<<"enter element number"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i =0; i<n; i++) {
+        cin>>arr[i];
+    }
+    cout<<"enter the key value to search"<<endl;
+    int x;
+    cin>>x;
+    sort(arr,arr+n);
+    int low = arr[0],high = arr[n-1];
 
-for(int i =0;i<n;i++)
-{
-cin>>arr[i];
+    while(low<=high) {
+        int mid = low+(high - low)/2;
+        if(arr[mid]==x) cout<<x;
+        if(arr[mid]<x) low = mid+1;
+        else high =mid-1;
 
-}
+
+    }
+
+
+return -1;
+
+
 }
